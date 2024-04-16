774bd8a217d6c023ec816d3f601c1d6900457f1d
PinkWolf457 <160530821+PinkWolf457@users.noreply.github.com>
2024-04-16 20:22:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const loader = document.getElementById("loading");
+  const mainContent = document.getElementById("main");
+
+  if (!localStorage.getItem("isLoaded")) {
+    loader.style.display = "block";
+    loader.style.opacity = "1";
+
+    const duration = 1000;
+    setTimeout(() => {
+      loader.style.opacity = "0";
+
+      setTimeout(() => {
+        loader.style.backgroundColor = "transparent";
+      }, 250);
+
+      setTimeout(() => {
+        loader.style.display = "none";
+        mainContent.classList.add("show");
+        localStorage.setItem("isLoaded", "true");
+      }, 500);
+    }, duration);
+  } else {
+    loader.style.display = "none";
+    mainContent.classList.add("show");
+  }
+});
